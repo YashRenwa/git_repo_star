872f4b8a1924dc7f6e232fd3047a32e04cb54e80
Yash Renwa <yash.renwa@gmail.com>
2023-10-27 13:41:50 +0530

    imports cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:git_repo_star/constants.dart';
 import 'package:git_repo_star/view/most_starred_repo_screen.dart';
-import 'package:git_repo_star/view/most_starred_repo_screen.dart';
 
 void main() {
   runApp(const MyApp());
